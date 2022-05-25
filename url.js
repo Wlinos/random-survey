f22b6dd51344a130acb99f9ca7b01542a7761c2c
Wlinos <106230413+Wlinos@users.noreply.github.com>
2022-05-25 15:27:45 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/zRmnBCBzwVYdAzfw9',
+	'https://forms.gle/XpkPs6RPzJsofFXN9',
 ];
